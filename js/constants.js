7da4034ca4612c331597f97e2add67ea94850c60
Jidgnesh <Jidgnesh@users.noreply.github.com>
2026-04-08 05:03:29 +0530

    feat: add CSS foundation with theme tokens, easing curves, and animation keyframes

--- js/constants.js
+++ js/constants.js
@@ -17,3 +17,4 @@ const PRIORITIES = {
   const TRASH_KEY = 'planner-trash-v1';
   const AUTH_KEY = 'planner-auth-v1';
   const USERS_KEY = 'planner-users-v1';
+  const THEME_KEY = 'planner-theme-v1';
